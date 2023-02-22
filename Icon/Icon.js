cfa28c6b50221e61e58fa964fceee9d6a1446807
Irisloverain <t106ac2007@ntut.org.tw>
2023-02-22 19:31:46 +0800

    2/22

---
+++ Icon/Icon.js
@@ -0,0 +1,35 @@
+import { ReactComponent as Bin } from './bin.svg'
+import { ReactComponent as Delete } from "./delete.svg";
+
+import { ReactComponent as Checkbox } from './checkbox.svg'
+import { ReactComponent as CheckboxChecked } from './checkboxChecked.svg'
+import { ReactComponent as less } from "./less.svg";
+import { ReactComponent as more } from "./more.svg";
+
+import { ReactComponent as Class } from "./class.svg";
+import { ReactComponent as Product } from "./product.svg";
+import { ReactComponent as About } from "./about.svg";
+import { ReactComponent as Member } from "./member.svg";
+
+import { ReactComponent as Vegtable } from "./vegtable.svg";
+import { ReactComponent as Fruit } from "./fruit.svg";
+import { ReactComponent as Process } from "./process.svg";
+import { ReactComponent as Others } from "./others.svg";
+
+
+export default {
+  Bin,
+  Checkbox,
+  CheckboxChecked,
+  less,
+  more,
+  Class,
+  Product,
+  About,
+  Member,
+  Delete,
+  Vegtable,
+  Fruit,
+  Process,
+  Others,
+};
